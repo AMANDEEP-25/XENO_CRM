dee1ac6578b22d271077609a4fbb86158554d0c8
Amandeep Singh <jyotsnaaman2003@gmail.com>
2024-11-15 19:54:47 +0530

    first commit

---
+++ backend/routes/customerRoutes.js
@@ -0,0 +1,16 @@
+// customerRoutes.js
+const express = require("express");
+const router = express.Router();
+const Customer = require("../models/Customer");
+
+router.post("/", async (req, res) => {
+  try {
+    const customer = new Customer(req.body);
+    await customer.save();
+    res.status(201).json(customer);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+module.exports = router;
